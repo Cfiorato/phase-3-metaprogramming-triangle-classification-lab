7b2644038171dab78c76e6428a3fa66421b2af39
Cameron Fiorato <cameronclayfiorato@gmail.com>
2022-01-14 09:28:52 -0600

    commit tree 25d6ad8fa7d260aeb7bb11a98fcaba770f08f21b

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
   # write code here
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind 
+    validate_triangle(@side1, @side2, @side3)
+    if @side1 == @side2 && @side2 == @side3
+      return :equilateral
+    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+      return :isosceles
+    else 
+      return :scalene
+    end
+  end
+
+  def validate_triangle(side1, side2, side3)
+    if side1 > 0 && side2 > 0 && side3 > 0
+      if side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2
+        return "valid triangle"
+      else
+        raise TriangleError
+      end
+    else 
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
